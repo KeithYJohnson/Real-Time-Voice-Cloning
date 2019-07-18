43d98870b5e5af4e99ea8a4d2289e7d536126313
ItsGiusto <giustobgood@optonline.net>
2019-07-18 13:02:35 -0400

    started interface for vocoder

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -1,4 +1,9 @@
 import flask
+
+from vocoder_caller.vocoder_caller import VocoderCaller
+
+
+vocoderCaller = VocoderCaller()
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -9,4 +14,5 @@ def root_route():
 def convert_text_to_celebrity_speech():
     text = flask.request.args.get('text')
     celebrity = flask.request.args.get('celebrity')
+    vocoderCaller.call_vocoder(text)
     return 'You sent text: {} celebrity: {}'.format(text, celebrity)
---
+++ vocoder_caller/vocoder_caller.py
@@ -0,0 +1,7 @@
+
+class VocoderCaller(object):
+    def __init__(self):
+        pass
+
+    def call_vocoder(self, text):
+        print("call_vocoder text: {}".format(text))
