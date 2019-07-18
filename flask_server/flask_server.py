b4ecfa04eb9c0d92ca66cfd0220c2e87919a6c14
ItsGiusto <giustobgood@optonline.net>
2019-07-18 12:37:32 -0400

    moved script, added args

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root_route():
+    return 'Welcome'
+
+@app.route('/convert_text_to_celebrity_speech')
+def convert_text_to_celebrity_speech():
+    text = flask.request.args.get('text')
+    celebrity = flask.request.args.get('celebrity')
+    return 'You sent text: {} celebrity: {}'.format(text, celebrity)
--- flask_server/start_server.sh
+++
@@ -1,2 +0,0 @@
-export FLASK_APP=flask_server.py
-flask run --host=0.0.0.0 --port=80
